8cc3faa854b84aa59b2ca79f3c4badc7f428027e
luismiphita <77441028+luismiphita@users.noreply.github.com>
2022-06-06 10:11:32 +0200

    a

--- 01_inicio/utils/js/variables.js
+++ 01_inicio/utils/js/variables.js
@@ -5,10 +5,17 @@
 
 const DNI = '123456789D';
 let nombre = "Luismi";
-let apellido;
+let apellido = undefined;
 let edad = 20;
 let carnet = true;
 let fecha = new Date();
 
 apellido = "Perez";
-DNI = 12345678;
+/* DNI = 12345678; */
+console.log(typeof nombre);
+console.log(typeof apellido);
+console.log(typeof edad);
+console.log(typeof carnet);
+console.log(typeof edad);
+console.log(typeof fecha);
+
