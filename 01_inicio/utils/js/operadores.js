4dd9a6df717215a2ea3bf7b56d1a8d3286dd4d8f
luismiphita <77441028+luismiphita@users.noreply.github.com>
2022-06-06 10:46:44 +0200

    l

--- 01_inicio/utils/js/operadores.js
+++ 01_inicio/utils/js/operadores.js
@@ -1,4 +1,4 @@
-// aritmeticos
+// operadores aritmeticos
 
 let numero1 = 6;
 let numero2 = 10;
@@ -31,15 +31,23 @@ console.log(`EL tipo del resto es ${typeof (numero1 % numero2)}`);
 
 numero1 = 11;
 numero2 = 10;
+
 let mayorQue = numero1 > numero2;
 console.log(mayorQue);
+
 let mayorIgual = numero1 >= numero2;
 console.log(mayorIgual);
+
 let menorQue = numero1 < numero2;
 console.log(menorQue);
+
 let menorIgual = numero1 <= numero2;
 console.log(menorIgual);
+
 let diferente = numero1 != numero2;
 console.log(diferente);
+
+numero1 = 4;
+numero2 = '4';
 let igual = numero1 == numero2;
 console.log(igual);
