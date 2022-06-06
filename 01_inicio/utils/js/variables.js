f3560319b2002f3a26c132d1eecdff956345fb2a
luismiphita <77441028+luismiphita@users.noreply.github.com>
2022-06-06 10:06:24 +0200

    Initial commit

---
+++ 01_inicio/utils/js/variables.js
@@ -0,0 +1,14 @@
+/* console.log('Archivo de variables cargado'); */
+// let --> ambito global y de clase
+// var --> ambito global --> NO SE UTILIZA
+// cons --> constantes
+
+const DNI = '123456789D';
+let nombre = "Luismi";
+let apellido;
+let edad = 20;
+let carnet = true;
+let fecha = new Date();
+
+apellido = "Perez";
+DNI = 12345678;
