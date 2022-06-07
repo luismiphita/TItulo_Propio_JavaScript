c89892a7870f5139c2cd4cea6748ded2d783a15b
luismiphita <77441028+luismiphita@users.noreply.github.com>
2022-06-07 09:38:03 +0200

    f

--- 01_inicio/utils/js/estructuras.js
+++ 01_inicio/utils/js/estructuras.js
@@ -9,13 +9,40 @@ else{
     console.log("La respuesta es false");
 }
 
-let nota = 4;
-if (nots >= 5) {
+/* let nota = 4;
+if (nota >= 5) {
     console.log("El examen esta aprobado");
 } else{
     console.log("El examen esta suspenso");
-}
+} */
 
 //if ternario
 //condicion ? true : false
 acierto ? console.log("Examen aprobado") : console.log("Examen suspenso");
+//if else
+//if else if else
+
+// con & solo hace lo que le pides, si estas aprobado bien, si no, no hace nada por el suspenso
+acierto & console.log("Examen aprobado")
+
+//switch
+
+/* let nota = 6;
+switch (nota) {
+    case 1:
+        console.log("dato introducido 1");
+        break;
+    case 2:
+        console.log("dato introducido 2");
+        break;
+    case 3:
+        console.log("dato introducido 3");
+        break;
+
+    default:
+        console.log("Dato incorrecto");
+        break;
+} */
+
+
+
