6867b98ac6da212b1b4522cf74b079509cec2e0a
luismiphita <77441028+luismiphita@users.noreply.github.com>
2022-06-07 09:26:49 +0200

    aa

---
+++ 01_inicio/utils/js/estructuras.js
@@ -0,0 +1,21 @@
+// if if else switch while do while for foreach (especial) for or for on break goto
+
+//if if else
+
+let acierto = true;
+if(acierto)
+console.log("La respuesta es true");
+else{
+    console.log("La respuesta es false");
+}
+
+let nota = 4;
+if (nots >= 5) {
+    console.log("El examen esta aprobado");
+} else{
+    console.log("El examen esta suspenso");
+}
+
+//if ternario
+//condicion ? true : false
+acierto ? console.log("Examen aprobado") : console.log("Examen suspenso");
