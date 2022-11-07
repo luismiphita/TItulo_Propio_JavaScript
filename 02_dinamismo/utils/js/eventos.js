9b19c255fa4a71f69de696daf75b5bc459b57cc3
luismiphita <luismiphita@gmail.com>
2022-11-07 10:22:13 +0100

    update

---
+++ 02_dinamismo/utils/js/eventos.js
@@ -0,0 +1,33 @@
+let boton_agregar = document.querySelector("#boton_agregar");
+let input_agregar = document.querySelector("#texto_li");
+let contador = 0;
+let spanPulsaciones = document.querySelector("span");
+
+boton_agregar.addEventListener("click", (element) => {
+  let contenido = input_agregar.value;
+  // console.log(contenido);
+  // agregar nodos
+  /*let lista = document.querySelector("#lista_primeros");
+  let nodoLI = document.createElement("li");
+  nodoLI.innerText = contenido;
+  //nodoLI.classList.add("list-group-item");
+  nodoLI.setAttribute("class", "list-group-item");
+  nodoLI.setAttribute("id", "valor");
+  lista.appendChild(nodoLI);*/
+  // modificar el html
+  let lista = document.querySelector("#lista_primeros");
+  lista.innerHTML = `${lista.innerHTML} <li class='list-group-item'>${contenido}</li>`;
+});
+
+input_agregar.addEventListener("keydown", (event) => {
+  // sacar la tecla pulsada por consola
+  // console.log(event.key);
+  // console.log(input_agregar.value);
+  contador++;
+  spanPulsaciones.innerText = contador;
+});
+
+/*  el en parrafo que está configurado al final en el 0
+ deberá aparecer el número de pulsaciones realizadas (dinámico)
+ declarar la funcion fuera (va a ser reutilizada?)
+ o dentro(solo ejecutada por el evento) --> anónima */
---
+++ proyecto_bj/utils/js/logica_black.js
@@ -0,0 +1,47 @@
+let baraja = [];
+
+function iniciarBaraja() {
+  let palos = ["T", "C", "D", "P"];
+
+  for (let index = 0; index < palos.length; index++) {
+    for (let index2 = 1; index2 < 14; index2++) {
+      if (index2 == 11) {
+        baraja.push("J" + palos[index]);
+      } else if (index2 == 12) {
+        baraja.push("Q" + palos[index]);
+      } else if (index2 == 13) {
+        baraja.push("K" + palos[index]);
+      } else {
+        baraja.push(index2 + palos[index]);
+      }
+    }
+  }
+}
+
+function mostrarBaraja() {
+  console.log(baraja);
+}
+
+iniciarBaraja();
+baraja = _.shuffle(baraja);
+mostrarBaraja();
+
+// este numero será la posicion de la carta que obtengo
+// sacar la carta obtenida y su valor Carta: 10J Valor: 10
+// sacar la carta obtenida y su valor Carta: 1J Valor: 1
+// sacar la carta obtenida y su valor Carta: KJ Valor: 10
+
+function calcularValor(carta) {
+  return carta.substring(0, carta.length - 1);
+}
+
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+console.log(calcularValor(baraja.pop(parseInt(Math.random() * 53))));
+
+console.log(baraja);
+
+nombre = prompt(`Introduce tu nombre`);
